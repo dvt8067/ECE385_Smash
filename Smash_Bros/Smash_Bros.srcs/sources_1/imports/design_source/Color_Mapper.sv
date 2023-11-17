d931ba8515a673037e554d9d5ec6e62a80925df7
Eli Hoon <elijahhoon@wirelessprv-10-195-120-105.near.illinois.edu>
2023-11-16 19:27:17 -0600

    hoping the background now works

--- Smash_Bros/Smash_Bros.srcs/sources_1/imports/design_source/Color_Mapper.sv
+++ Smash_Bros/Smash_Bros.srcs/sources_1/imports/design_source/Color_Mapper.sv
@@ -20,17 +20,20 @@ module  color_mapper ( input  logic [9:0] MarioX, MarioY, DrawX, DrawY, Mario_si
                        input logic Mario_Invert_Left,
                        output logic [3:0]  Red, Green, Blue,
                        output logic[9:0] Stage_X_Max, Stage_X_Min, Stage_Y_Max, Stage_Y_Min);
-    
+
     logic Mario_on;
     logic [12:0] Mario_address, ADDR0X_Mario, ADDR0Y_Mario, ADDR_X_OFFSET_Mario, ADDR_Y_OFFSET_Mario;
-    logic [1:0] Palette_Index_Stationary;
-    logic [1:0] Palette_Index_Walking1, Palette_Index_Walking2, Palette_Index_Walking3;
-    logic [1:0] Palette_Index;
+    logic [3:0] Palette_Index_Stationary;
+    logic [3:0] Palette_Index_Walking1, Palette_Index_Walking2, Palette_Index_Walking3;
+    logic [3:0] Palette_Index;
+    logic[3:0] Background_palette index;
     logic [11:0] Palette_Output;
+    logic [18:0] background_address;
+    logic on_background;
     //logic [9:0] Stage_X_Max, Stage_X_Min, Stage_Y_Max, Stage_Y_Min;
-	 
- 
-	  
+
+
+
     int Size_X, Size_Y;
     assign Size_X = Mario_size_X;
     assign Size_Y = Mario_size_Y;
@@ -45,41 +48,44 @@ module  color_mapper ( input  logic [9:0] MarioX, MarioY, DrawX, DrawY, Mario_si
     assign Mario_Rowsize = 60;
     assign ADDR0X_Mario = MarioX - DistX_Mario;
     assign ADDR0Y_Mario = MarioY - DistY_Mario;
+    assign background_address = (DrawY*640) + DrawX;
+    assign on_background = 0;
     always_comb begin
         ADDR_X_OFFSET_Mario = DrawX - ADDR0X_Mario;
         ADDR_Y_OFFSET_Mario = DrawY - ADDR0Y_Mario;
         if(Mario_Invert_Left == 0) begin
-            Mario_address = ADDR_X_OFFSET_Mario + ADDR_Y_OFFSET_Mario*Mario_Rowsize; // mario is right 
+            Mario_address = ADDR_X_OFFSET_Mario + ADDR_Y_OFFSET_Mario*Mario_Rowsize; // mario is right
         end
         else begin
             Mario_address = (60-ADDR_X_OFFSET_Mario) + ADDR_Y_OFFSET_Mario*Mario_Rowsize;
         end
     end
-    
-    
-    
+
+
+
     Mario_Stationary_RAM Mario_Stationary_ROM (
 		.read_address(Mario_address),
 		.Clk(Clk),
 		.data_Out(Palette_Index_Stationary)
     );
 
-    Mario_Walking_RAM1 Mario_Walking1_ROM ( 
+    Mario_Walking_RAM1 Mario_Walking1_ROM (
 		.read_address(Mario_address),
 		.Clk(Clk),
 		.data_Out(Palette_Index_Walking1)
     );
-    Mario_Walking_RAM2 Mario_Walking2_ROM ( 
+    Mario_Walking_RAM2 Mario_Walking2_ROM (
 		.read_address(Mario_address),
 		.Clk(Clk),
 		.data_Out(Palette_Index_Walking2)
     );
-    Mario_Walking_RAM3 Mario_Walking3_ROM ( 
+    Mario_Walking_RAM3 Mario_Walking3_ROM (
 		.read_address(Mario_address),
 		.Clk(Clk),
 		.data_Out(Palette_Index_Walking3)
     );
-    
+    Background Smash_Background(read_address(background_address), .Clk, .data_Out(Background_palette));
+
     always_comb begin
         if(Mario_State_Out == 0 || Mario_State_Out == 1 || Mario_State_Out == 8 || Mario_State_Out == 9)begin
             Palette_Index = Palette_Index_Stationary;
@@ -97,42 +103,49 @@ module  color_mapper ( input  logic [9:0] MarioX, MarioY, DrawX, DrawY, Mario_si
             Palette_Index = 2'b01; // if for some reason we are not in one of the defined states, print red
         end
     end
+    if(on_background) begin
+    Palette_Index = Background_palette;
+    end
+    else begin
+    Palette_Index = Palette_Index;
+    end
     Palette(.addr(Palette_Index), .data(Palette_Output));
-  
+
     always_comb
     begin:Mario_on_proc
         if ((DrawX >= MarioX - Mario_size_X) &&
             (DrawX <= MarioX + Mario_size_X) &&
             (DrawY >= MarioY - Mario_size_Y) &&
             (DrawY <= MarioY + Mario_size_Y)) begin
-       
+
             Mario_on = 1'b1;
        end
-        else 
+        else
             Mario_on = 1'b0;
-     end 
-       
+     end
+
     always_comb
     begin:RGB_Display
         if(Mario_on && (Palette_Output != 12'h808)) begin
              Red = Palette_Output[11:8];
              Green = Palette_Output[7:4];
              Blue = Palette_Output[3:0];
-        end  
-        else if ((DrawY>Stage_Y_Min) && (DrawY<Stage_Y_Max)&& (DrawX<Stage_X_Max) && (DrawX>Stage_X_Min)) begin 
+        end
+        else if ((DrawY>Stage_Y_Min) && (DrawY<Stage_Y_Max)&& (DrawX<Stage_X_Max) && (DrawX>Stage_X_Min)) begin
             Red = 4'hf;
             Green = 4'h7;
             Blue = 4'h0;
         end
-   
-        else begin 
-            Red = 4'hf; 
-            Green = 4'hf;
-            Blue = 4'hf;
-        end      
-    end 
 
-      
-   
-    
+        else begin
+            on_background = 1;
+            Red = Palette_Output[11:8];
+            Green = Palette_Output[7:4];
+            Blue = Palette_Output[3:0];
+        end
+    end
+
+
+
+
 endmodule
---
+++ Smash_Bros/Smash_Bros.srcs/sources_1/imports/design_source/Smash_Background.sv
@@ -0,0 +1,29 @@
+/*
+ * ECE385-HelperTools/PNG-To-Txt
+ * Author: Rishi Thakkar
+ *
+ */
+
+module Smash_Background
+(
+
+		input [18:0] read_address,
+		input Clk,
+
+		output logic [2:0] data_Out
+);
+
+// mem has width of 2 bits and a total of 4800 addresses
+logic [1:0] mem [0:307199];
+
+initial
+begin
+	 $readmemh("smash_bros_background_2_final.txt", mem);
+end
+
+
+always_ff @ (posedge Clk) begin
+	data_Out<= mem[read_address];
+end
+
+endmodule
